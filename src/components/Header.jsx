35922c8d35ba59b4600ca06592178f6ee33c6197
Asad <imasad012@gmail.com>
2024-02-19 20:01:45 +0500

    animation added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,15 @@ import TelegramImg from '/src/assets/images/telegram.png'
 
 const Header = () => {
   return (
-    <div className='pt-[1.5vw] pb-[0.5vw] px-[5vw] flex justify-between items-center'>
-        <div className='chibold text-theme stroke1 md:text-stroke-black-1-5 text-[5vw] md:text-[3vw] leading-[-1.76px] transition-all duration-1000'>BOBO THE BEAR Jr</div> 
+    <div className='pt-[1.5vw] pb-[0.5vw] px-[5vw] flex justify-between items-center headerTop'>
+        <a href="/" className='chibold text-theme stroke1 md:text-stroke-black-1-5 text-[5vw] md:text-[3vw] leading-[-1.76px] transition-all duration-1000'>BOBO THE BEAR Jr</a> 
 
         <div className='flex justify-end items-center'>
-            <a href='https://twitter.com/BoboJr_SOL' target='_blank'>
-                <img src={TwitterImg} alt='Twitter' className='w-[40px]'/>
+            <a href='https://twitter.com/BoboJr_SOL' target='_blank' className='bounceDown'>
+                <img src={TwitterImg} alt='Twitter' className='w-[50px]'/>
             </a>
-            <a href='https://t.me/BoboJr_SOL' target='_blank'>
-                <img src={TelegramImg} alt='Telegram' className='w-[40px]'/>
+            <a href='https://t.me/BoboJr_SOL' target='_blank' className='bounceDown'>
+                <img src={TelegramImg} alt='Telegram' className='w-[50px]'/>
             </a>
         </div>
     </div>

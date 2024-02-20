3a029fb879dcdf0c53323881ac0601695e0edb22
Asad <imasad012@gmail.com>
2024-02-20 21:15:28 +0500

    header hot fixes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import TelegramImg from '/src/assets/images/telegram.png'
 
 const Header = () => {
   return (
-    <div className='pt-[1.5vw] pb-[0.5vw] px-[5vw] flex justify-between items-center headerTop'>
-        <a href="/" className='chibold text-theme stroke1 md:text-stroke-black-1-5 text-[5vw] md:text-[3vw] leading-[-1.76px] transition-all duration-1000'>BOBO THE BEAR Jr</a> 
+    <div className='pt-[1.5vw] pb-[0.5vw] px-[5vw] flex justify-between items-center headerTop mt-[-5px]'>
+        <a href="/" className='chibold text-theme hover:text-[#2e7d32] strokehead md:text-stroke-black-1-5 text-[5vw] md:text-[3vw] leading-[-1.76px] transition-all duration-1000 tracking-[-1px]  '>BOBO THE BEAR Jr</a> 
 
         <div className='flex justify-end items-center'>
             <a href='https://twitter.com/BoboJr_SOL' target='_blank' className='bounceDown'>

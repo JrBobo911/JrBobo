1e73be4925444049b8619f24df3069b5df54023c
Asad <imasad012@gmail.com>
2024-02-22 10:09:04 +0500

    pinksale link changed

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             <img src={BtnImg1} alt=""/>
             <span className="chibold text-[4.5vw] md:text-[2vw]  text-[#D1FFC8] font-[500] transition-all duration-300 text-center stroke1 spacing-[0.1vw] mt-[-4.5vw] absolute top-[7.5vw] md:top-[6.2vw]">CHART</span>
           </a>
-          <a href="https://www.pinksale.finance/" target="_blank" className="w-[150px] md:w-[17.5vw] flex  justify-center items-center relative">
+          <a href="https://beta.pinksale.finance/solana/launchpad/DWGSJDdDnLdhWsBUFiKSt7DvGwBPuSxrkk2zUJZbdgEY" target="_blank" className="w-[150px] md:w-[17.5vw] flex  justify-center items-center relative">
             <img src={BtnImg2} alt=""/>
             <span className="chibold text-[4.5vw] md:text-[2vw]  text-[#BFFF3A] font-[500] transition-all duration-300 text-center stroke1 spacing-[0.1vw] mt-[-4.5vw] absolute top-[7.5vw] md:top-[6.2vw]">BUY NOW</span>
           </a>
